e5e204432553696a2c0ea7f707dd73fc8a1dff9c
Steve Denton <metasapien@gmail.com>
2018-09-21 11:57:46 +0100

    Commit test

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import static org.junit.Assert.assertTrue;
+
+public class FirstTest {
+
+    @Test
+
+    public void driverIsKing() {
+
+        WebDriver driver = new HtmlUnitDriver();
+
+        driver.get("http://www.compendiumdev.co.uk/selenium");
+
+        assertTrue(driver.getTitle().startsWith("Selenium Simplified"));
+
+    }
+}
