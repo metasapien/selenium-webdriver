8bb317a4e8111bdc1c723e8b57219559738b9db0
Steve Denton <metasapien@gmail.com>
2018-09-21 12:04:50 +0100

    Commit test

---
+++ src/test/java/JUnitBeforeAndAfterTest.java
@@ -0,0 +1,39 @@
+import org.junit.*;
+
+public class JUnitBeforeAndAfterTest {
+
+    @BeforeClass
+    public static void beforeClass(){
+        System.out.println("Before class");
+    }
+
+    @Before
+    public void beforeTest(){
+        System.out.println("Before test");
+    }
+
+    @Test
+    public void this1Test() {
+        System.out.println("This is 1 test");
+    }
+
+    @Test
+    public void this2Test() {
+        System.out.println("This is 2 test");
+    }
+
+    @Test
+    public void this3Test() {
+        System.out.println("This is 3 test");
+    }
+
+    @After
+    public void afterTest(){
+        System.out.println("After test");
+    }
+
+    @AfterClass
+    public static void afterClass(){
+        System.out.println("After class");
+    }
+}
